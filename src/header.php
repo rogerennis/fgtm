b65c81556281a512f9ec7d60ebd708a3d2455185
Roger <roadene@gmail.com>
2016-02-10 22:40:00 -0500

    Added branding elements

--- src/header.php
+++ src/header.php
@@ -28,24 +28,24 @@
 
 		<!-- wrapper -->
 		<div class="wrapper">
-
-			<!-- header -->
-			<header class="header clear" role="banner">
-
-					<!-- logo -->
+            <!-- nav -->
+                <nav class="nav" role="navigation">
+                    <!-- LOGO -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/fgtm_logo.svg" alt="Logo" class="logo-img">
 						</a>
 					</div>
-					<!-- /logo -->
+					<!-- /LOGO -->
+					
+				    <?php html5blank_nav(); ?>
+				</nav>
+            <!-- /nav -->
+			
+			<!-- header -->
+			<header class="header clear" role="banner">
 
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
 
 			</header>
 			<!-- /header -->
