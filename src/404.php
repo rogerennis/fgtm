d2a465fe5e59092ed55b5c60080beee2cb8bdbe5
Roger <roadene@gmail.com>
2016-04-20 00:17:55 -0400

    404 responsive. nav adjustments.

--- src/404.php
+++ src/404.php
@@ -1,24 +1,35 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
-
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<main role="main" class="four-o-four-container">
+  <!-- section -->
+  <section>
+    <div class="wrapper">
+      <!-- article -->
+      <article id="post-404" class="four-o-four">
+        <h1>404</h1>
+        <div class="message">
+          <h2>Ask,</h2>
+          <h4>and it will be given to you;</h4>
+          
+          <h2>Seek,</h2>
+          <h4>and you will find;</h4>
+          
+          <h2>Knock,</h2>
+          <h4>and it will be opened to you.</h4>
+        </div>
+        
+        <div class="cta">
+          <p class="verse">Matthew 7:7 NKJV</p>
+          
+          <p class="btn-copy">You can always...</p>
+          <a class="fgtm-404-btn" href="<?php echo home_url(); ?>"><?php _e( 'Return home', 'fgtm-theme' ); ?></a>
+          
+        </div>
+      </article>
+      <!-- /article -->
+    </div>
+  </section>
+  <!-- /section -->
+</main>
 
 <?php get_footer(); ?>
--- src/index.php
+++ src/index.php
@@ -1,21 +1,19 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<div class="wrapper">
-  		<!-- section -->
-  		<section>
-  
-  			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
-  
-  			<?php get_template_part('loop'); ?>
-  
-  			<?php get_template_part('pagination'); ?>
-  
-  		</section>
-  		<!-- /section -->
-		</div>
-	</main>
-
-<?php get_sidebar(); ?>
+	<div class="wrapper">
+  	<main role="main">
+    		<!-- section -->
+    		<section>
+    
+    			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+    
+    			<?php get_template_part('loop'); ?>
+    
+    			<?php get_template_part('pagination'); ?>
+    
+    		</section>
+    		<!-- /section -->
+  	</main>
+	</div>
 
 <?php get_footer(); ?>
