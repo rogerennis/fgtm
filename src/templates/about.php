7700b44fa7590ce461e61ddd2a1e676830a74777
Roger <roadene@gmail.com>
2016-06-15 00:36:11 -0400

    updated about page images and styles. updated nav for tablet + size screens.

--- src/templates/about.php
+++ src/templates/about.php
@@ -55,44 +55,44 @@
           
         <div class="leadership-container">
           <article class="leadership">
-            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/john_christine_williamson.JPG" alt="John & Christine Williamson" /></figure>
+            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/pastor_john_&_cristine_williamson.jpg" alt="pastors John & Christine Williamson" /></figure>
+            <h4>Rev. John & Cristine Williamson</h4>
             <p class="title">Founders & Senior Pastors</p>
-            <h4>Rev. John & Christine Williamson</h4>
             <p>Our founding pastors (born in N. Ireland) have been prayerfully and lovingly shepherding our fellowship since 1984. They provide support, counsel, and encouragement to those who are in need. They are proud parents of four sons, and also have ten grandchildren.</p>
           </article>
            
           <article class="leadership">
-            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/pastorBrianJag.jpg" alt="pastorBrianJag" /></figure>
-            <p class="title">Assistant Pastor</p>
+            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/pastor_brian_jag.jpg" alt="pastor Brian Jag" /></figure>
             <h4>Rev. Brian Jagnanan</h4>
+            <p class="title">Assistant Pastor</p>
             <p>Pastor Brian has been preaching the Gospel for since 2003. Born on the Island of Trinidad and Tobago and migrated to Canada since 1991. As full time Assistant Pastor at FGTM my role is preaching, teaching, praying and interceding on behalf of others. Pastor Brian is married with two beautiful children.</p>
           </article>
            
           <article class="leadership">
-            <figure><img src="#" /></figure>
-            <p class="title">Youth Leaders</p>
+            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/pastors_delando_&_nadine_henry.jpg" alt="pastors Delando & Nadine Henry"/></figure>
             <h4>Delando & Nadine Henry</h4>
+            <p class="title">Youth Leaders</p>
             <p>Delando & Nadine Henry are originally from Kingston, Jamaica. They have been youth and camp leaders at their church there for many years. They are proud parents of 3 children. Both Delando and Nadine play vital roles in guiding our church youth.</p>
           </article>
            
           <article class="leadership">
-            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/carolCarey.jpg" alt="carolCarey" /></figure>
-            <p class="title">Church Secretary</p>
+            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/carol_carey.jpg" alt="Carol Carey" /></figure>
             <h4>Carol Carey</h4>
+            <p class="title">Church Secretary</p>
             <p>If you are inquiring about our fellowship, IF you are looking for a church, IF you need spiritual assistance, prayer and support, <strong>call (905) 459-HOPE</strong>. You will be greeted by a friendly voice and a caring spirit. Drop by or call to make an appointment. Regular office hours, 9:00 am -5:30 pm, Monday-Friday.</p>
           </article>
            
           <article class="leadership">
             <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/pastorRiptonRamsingh.jpg" alt="pastorRiptonRamsingh" /></figure>
-            <p class="title">Pastor</p>
             <h4>Ripton Ramsingh</h4>
+            <p class="title">Pastor</p>
             <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod.</p>
           </article>
            
           <article class="leadership">
-            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/pastorMonicaMills.jpg" alt="pastorMonicaMills" /></figure>
-            <p class="title">Pastor</p>
+            <figure><img src="<?php echo get_template_directory_uri(); ?>/img/leadership/pastor_monica_mills.jpg" alt="pastor Monica Mills" /></figure>
             <h4>Monica Mills</h4>
+            <p class="title">Pastor</p>
             <p>Born in Jamaica she has been in canada for Forty years. she is a wife, mother of four children and one grandchild. Since 1997 she has dedicated her life to the ministry of Jesus Christ. Her personal testimonies of God’s miracle and healing power is an encouragement to all. She is a preacher and a speaker, and is passionate about the Ladies Ministry. She has been with Faith Gospel Tabernacle Ministries for ten years.</p>
           </article>
         </div>
